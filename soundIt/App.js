b6fe78e0f506e840ad63134941931ef0a5b505d3
ironpup <christoph.ding@gmail.com>
2018-04-01 14:22:10 -0400

    Refactor client application to use my container component for the entire application.

---
+++ soundIt/App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Application from './client/entireApplication';
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Application />
+    );
+  }
+}
+
+
---
+++ soundIt/client/entireApplication.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet,
+  Text
+} from 'react-native';
+
+class Application extends Component {
+   render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.helper}>
+            Entire Application
+        </Text>      
+      </View>
+    )
+  }
+}
+
+export default Application
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  helper: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    backgroundColor: 'skyblue'
+  }
+});
