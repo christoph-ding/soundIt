88db02bc119aaf98f5028e3d5c9b5b044e0df8a3
ironpup <christoph.ding@gmail.com>
2018-04-15 22:00:59 -0400

    Refactor and clean up some in the friends page.

--- soundIt/client/friends/friends.js
+++ soundIt/client/friends/friends.js
@@ -5,8 +5,6 @@ import {
   Text
 } from 'react-native';
 
-import ActionButton from '../ui/actionButton';
-import Banner from '../ui/banner';
 import FriendList from './friendList';
 
 const testFriends = ['Friend One', 
@@ -17,9 +15,6 @@ class Friends extends Component {
    render() {
     return (
       <View style={styles.container}>
-        <ActionButton title='Navigate 1'/>
-        <Banner title='Friends'/>
-        <ActionButton title='Navigate 2'/>
         <FriendList friends={testFriends}/>
       </View>
     )
@@ -30,7 +25,7 @@ export default Friends
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
+    flex: 1,
     // justifyContent: 'center',
     // alignItems: 'center',
     width: '100%',
