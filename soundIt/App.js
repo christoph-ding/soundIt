78083ba8300b0692556b46abaa1d48edb304d9b5
ironpup <christoph.ding@gmail.com>
2018-05-06 21:38:20 -0400

    Add aws-amplify to app.js.

--- soundIt/App.js
+++ soundIt/App.js
@@ -9,6 +9,13 @@ import React, { Component } from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
 
+// AWS Specific
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import {awsmobile} from './aws-exports';
+
+Amplify.configure(awsmobile);
+
 // my modules
 import rootReducer from './client/reducers'
 import Application from './client/entireApplication';
