f6469452c9fe3dadfc07bf5b583cfa752d771f28
ironpup <christoph.ding@gmail.com>
2018-05-09 22:21:51 -0400

    Wrap the application in an authentication component.

--- soundIt/App.js
+++ soundIt/App.js
@@ -1,29 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 // libraries
-import React, { Component } from 'react';
-import { createStore } from 'redux';
+import React, { Component } from 'react'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 // AWS Specific
-import Amplify from 'aws-amplify';
-import { withAuthenticator } from 'aws-amplify-react-native';
-import {awsmobile} from './aws-exports';
+import Amplify from 'aws-amplify'
+import { withAuthenticator } from 'aws-amplify-react-native'
+import {awsmobile} from './aws-exports'
 
 Amplify.configure(awsmobile);
 
 // my modules
 import rootReducer from './client/reducers'
-import Application from './client/entireApplication';
+import Application from './client/entireApplication'
+import WithAuth from './client/auth/withAuth'
 
-export default class App extends Component<Props> {
-  render() {
+class App extends Component {
+    render() {
+    console.log(this.props)
     return (
       <Application />
-    );
+    )
   }
 }
+
+export default WithAuth(App)
