5b5c836b1200513b80528538d774a81019f12d68
ironpup <christoph.ding@gmail.com>
2018-06-27 22:03:32 -0400

    Prepare the user sign up api.

--- awsmobilejs/backend/cloud-api/usersHandler/crud.js
+++ awsmobilejs/backend/cloud-api/usersHandler/crud.js
@@ -1,4 +1,8 @@
-exports.makeNewUser = function() {
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient({region:'us-east-1'});
+const uuid = require('uuid')
+
+exports.makeNewUser = function(event, context, callback) {
     
     console.log('=====================================')
     console.log('making a new user')
@@ -10,4 +14,19 @@ exports.makeNewUser = function() {
         },
         TableName : process.env.TABLE_NAME
     }
+    
+    ddb.put(params, function(err, data) {
+        if (err) {
+            context.done('error','putting item into dynamodb failed: '+err);
+        } else {
+            let response = {
+                    statusCode: 200,
+                    headers: {
+                        "x-custom-header" : "hey what's up"
+                    },
+                    body: JSON.stringify({"success": "get it back", "method": event.httpMethod})
+                };
+            context.succeed(response);
+        }
+    })
 }
--- awsmobilejs/backend/cloud-api/usersHandler/index.js
+++ awsmobilejs/backend/cloud-api/usersHandler/index.js
@@ -1,34 +1,13 @@
 'use strict';
-const AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient({region:'us-east-1'});
-const uuid = require('uuid')
 const crud = require('crud.js')
 
 exports.handler = function(event, context, callback) {
     console.log('=====================================')
-    console.log('event:')
+    console.log('event method: ')
+    console.log(event.httpMethod)
     console.log(event)
     
-    
-    
-    crud.makeNewUser()
-    // ddb.put(params, 
-    //     function(err, data) {
-    //         if (err) {
-    //             context.done('error','putting item into dynamodb failed: '+err);
-    //         } else {
-                
-    //         let response = {
-    //             statusCode: responseCode,
-    //             headers: {
-    //                 "x-custom-header" : "hey what's up"
-    //             },
-    //             body: JSON.stringify({"success": "get it back", "method": event.httpMethod})
-    //         };            
-                
-    //         console.log("=========================================")
-    //         console.log("response: " + JSON.stringify(response) + event.httpMethod)    
-    //         context.succeed(response);
-    //     }
-    // })
+    if (event.httpMethod === 'POST') {
+        crud.makeNewUser(event, context, callback)    
+    }
 };
