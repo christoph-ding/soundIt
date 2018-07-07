91a85e992f60aa2996f9b70e8f2381fb8b611851
ironpup <christoph.ding@gmail.com>
2018-07-07 16:47:25 -0400

    The api code is in a state in which the policy I made for group batches works with the database, but not neccesarily the batch write (yet).

--- awsmobilejs/backend/cloud-api/groupsHandler/groups-crud.js
+++ awsmobilejs/backend/cloud-api/groupsHandler/groups-crud.js
@@ -3,85 +3,90 @@ const ddb = new AWS.DynamoDB.DocumentClient({region:'us-east-1'});
 const uuid = require('uuid')
 
 exports.makeNewGroup = function(event, context, callback) {
-    const parsedBody = JSON.parse(event.body)
+    
+    console.log('event in crud: ', event)
+    // const parsedBody = JSON.parse(event.body)
     
     console.log('=========== GROUP ============')
     console.log('event: ', event)
     
-    console.log('=========== Oarsed ============')
-    console.log('parsedBody: ', parsedBody)
+    console.log('=========== Parsed ============')
+    // console.log('parsedBody: ', parsedBody)
+    
+    console.log('=========== Process Env ============')
+    // console.log(process.env)
     
     let TABLE_NAME = process.env.TABLE_NAME
-    
-    let params = {
-        RequestItems: {
-            TABLE_NAME: [
-                {
-                    "PutRequest": {
-                        "Item": { 
-                            EntityID: "group1",
-                            IndividualID: '123',
-                            Answered: false,
-                            Display: parsedBody.display
-                        }
-                    }
-                },
-                {
-                    "PutRequest": {
-                        "Item": {
-                            EntityID: "group1",
-                            IndividualID: '456',
-                            Answered: false,
-                            Display: parsedBody.display
-                        }
-                    }
-                }
-            ]
-        }
+  
+    const params = {
+        Item: {
+            EntityID: "user",
+            IndividualID: "Getting Batch To Work",
+            Answered: false,
+            Display: "Getting Batch To Work"
+        },
+        TableName: TABLE_NAME
     }
     
-    ddb.batchWrite(params, function(err, data) {
+    ddb.put(params, function(err, data) {
         if (err) {
-            console.log('========================')
-            console.log(err)
             context.done('error','putting item into dynamodb failed: '+err);
         } else {
             let response = {
-                statusCode: 200,
-                body: JSON.stringify(
-                    {"success": "you were successful", 
-                     "method": event.httpMethod,
-                     "event": event
-                    }
-                )}
+                    statusCode: 200,
+                    body: JSON.stringify(
+                        {"success": "you were successful", 
+                         "method": event.httpMethod,
+                         "event": event
+                    })
+                };
             context.succeed(response);
         }
-    });
+    })
 };
-    // context.succeed(response);
-    
-    //     const params = {
-    //     Item: {
-    //         EntityID: "user",
-    //         IndividualID: parsedBody.userID,
-    //         Answered: false,
-    //         Display: parsedBody.display
-    //     },
-    //     TableName: process.env.TABLE_NAME
-    // }
 
-    // ddb.put(params, function(err, data) {
+
+    // let params = {
+    //     RequestItems: {
+    //         TABLE_NAME: [
+    //             {
+    //                 "PutRequest": {
+    //                     "Item": { 
+    //                         EntityID: "group1",
+    //                         IndividualID: '123',
+    //                         Answered: false
+    //                         // Display: parsedBody.display
+    //                     }
+    //                 }
+    //             },
+    //             {
+    //                 "PutRequest": {
+    //                     "Item": {
+    //                         EntityID: "group1",
+    //                         IndividualID: '456',
+    //                         Answered: false
+    //                         // Display: parsedBody.display
+    //                     }
+    //                 }
+    //             }
+    //         ]
+    //     }
+    // }
+    
+    // ddb.batchWrite(params, function(err, data) {
     //     if (err) {
+    //         console.log('========================')
+    //         console.log(err)
     //         context.done('error','putting item into dynamodb failed: '+err);
     //     } else {
     //         let response = {
-    //                 statusCode: 200,
-    //                 body: JSON.stringify(
-    //                     {"success": "you were successful", 
-    //                      "method": event.httpMethod,
-    //                      "event": event
-    //                 })
-    //             };
+    //             statusCode: 200,
+    //             body: JSON.stringify(
+    //                 {"success": "you were successful", 
+    //                  "method": event.httpMethod,
+    //                  "event": event
+    //                 }
+    //             )}
     //         context.succeed(response);
     //     }
-    // })
+    // });
--- awsmobilejs/backend/cloud-api/groupsHandler/index.js
+++ awsmobilejs/backend/cloud-api/groupsHandler/index.js
@@ -2,6 +2,7 @@
 const groupsCRUD = require('groups-crud.js');
 
 exports.handler = function(event, context, callback) {
+    console.log('here: ', event)
     if (event.httpMethod === 'POST') {
         groupsCRUD.makeNewGroup(event, context, callback);
     }
