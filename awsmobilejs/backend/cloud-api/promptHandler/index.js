e02e7b4b307c3d0c251e0ed694bd057aedce2152
ironpup <christoph.ding@gmail.com>
2018-09-23 20:39:54 -0400

    Add a sorted scan of the prompts table.

--- awsmobilejs/backend/cloud-api/promptHandler/index.js
+++ awsmobilejs/backend/cloud-api/promptHandler/index.js
@@ -4,6 +4,6 @@ const promptsCRUD = require('prompts-crud.js')
 exports.handler = function(event, context, callback) {
     console.log('============================')
     if (event.httpMethod === 'GET') {
-        promptsCRUD.gettingMessages(event, context, callback)
+        promptsCRUD.getPrompt(event, context, callback)
     }
 }
--- awsmobilejs/backend/cloud-api/promptHandler/prompts-crud.js
+++ awsmobilejs/backend/cloud-api/promptHandler/prompts-crud.js
@@ -4,22 +4,32 @@ const ddb = new AWS.DynamoDB.DocumentClient({region:'us-east-1'})
 exports.getPrompt = function(event, context, callback) {
     console.log('=========== fetching prompts ===========')
 
-    let params = {
-        TableName : "soundit-mobilehub-1837399535-Prompt",
-        KeyConditionExpression: "",
-        ExpressionAttributeValues: {}
+    const params = {
+        TableName: "soundit-mobilehub-1837399535-Prompt",
+        KeyConditionExpression: "PromptID = :partition",
+        scanIndexForward: true,
+        ExpressionAttributeValues: {
+            ':partition': '1'
+        }
     }
     
     ddb.query(params, function(err, data) {
         if (err) {
             console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
         } else {
+            console.log(data)
+            data.Items.forEach((item) => {
+                console.log(item)
+            })
+            
             let response = {
                 statusCode: 200,
                 body: JSON.stringify({
                     "success": "you were successful",
                     "data": data
-                    })
+                })
             }
             context.succeed(response)
+        }
+    })
 }
