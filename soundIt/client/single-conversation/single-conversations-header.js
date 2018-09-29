3afee8bda2121462a8176b3c0891ff54cab977e2
ironpup <christoph.ding@gmail.com>
2018-09-29 00:16:50 -0400

    Stub out two more headers.

---
+++ soundIt/client/single-conversation/single-conversations-header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet,
+  Text
+} from 'react-native';
+
+import Banner from '../ui/banner';
+
+class GroupsHeader extends Component {
+  render() {
+    return (
+      <Banner title='Single COnversation'/>
+    )
+  }
+}
+export default GroupsHeader
