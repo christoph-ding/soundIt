2b271932c19a40075f9122ff63925cd4f046f229
ironpup <christoph.ding@gmail.com>
2018-11-27 22:08:03 -0500

    Now recording a message will update that table to say that user answered.

--- awsmobilejs/backend/cloud-api/answeredHandler/answered-crud.js
+++ awsmobilejs/backend/cloud-api/answeredHandler/answered-crud.js
@@ -39,28 +39,37 @@ exports.getAnsweredStatus = function(event, context, callback) {
 }
 
 exports.updateAnsweredStatus = function(event, context, callback) {
-    console.log('=========== making new user ===========')
+    console.log('=========== updating answered ===========')
     const parsedBody = JSON.parse(event.body)
-    console.log(parsedBody)  
+
+    const userID = parsedBody.userID
+    const groupID = parsedBody.groupID
+
+    const params = {
+        Key: {
+            "IndividualID": userID,
+            "EntityID": groupID
+        },
+        UpdateExpression: "set Answered = :a",
+        ExpressionAttributeValues: {
+            ":a": true
+        },
+        TableName: "soundit-mobilehub-1837399535-GroupsUsers"
+    }
     
-//     const params = {
-//         Item: {
-//         },
-//         TableName: "soundit-mobilehub-1837399535-GroupsUsers"
-//     }
-    
-//     ddb.put(params, function(err, data) {
-//         if (err) {
-//             context.done('error','putting item into dynamodb failed: '+err)
-//         } else {
-//             console.log('=========== successfully made new user ===========')
-//             let response = {
-//                 statusCode: 200,
-//                 body: JSON.stringify({
-//                     "success": "you were successful"
-//                 })
-//             }
-//             context.succeed(response)
-//         }
-//     })
+    ddb.update(params, function(err, data) {
+        if (err) {
+            console.log('======')
+            console.log(err)
+            context.done('error','putting item into dynamodb failed: '+err)
+        } else {
+            let response = {
+                statusCode: 200,
+                body: JSON.stringify({
+                    "success": "you were successful"
+                })
+            }
+            context.succeed(response)
+        }
+    })
 }
